c9518c3e40c2d2b1e1a8c80f37636d3a988c8523
AndreyArthur <twekylaywer@gmail.com>
2021-08-25 19:28:43 -0300

    fix: add core-js and regenerator-runtime to acces all javascript features

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
 import { App } from '@/App';
 
 import '@/globals.scss';
