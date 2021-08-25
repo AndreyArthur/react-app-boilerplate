af6cf241b6a1d3a2ee3220a7f35aeccef48ce25c
AndreyArthur <twekylaywer@gmail.com>
2021-08-25 18:14:05 -0300

    feat: reset css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import './globals.css'
+
 const App = () => <h1>React App</h1>;
 
 ReactDOM.render(<App />, document.getElementById('root'));
